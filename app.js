dac0563640a2c8ceecf0c2778a7d7dc3655fedb9
Junjie Jiang <junjiejiang94@gmail.com>
2017-04-25 16:07:29 -0700

    test

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ var app = express();
 app.use(compression());
 app.use(helmet());
 
+app.use('/assets', express.static(__dirname + '/assets')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
@@ -34,17 +40,9 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 
 app.listen(3000, function() {
