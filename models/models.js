349a06a4950ebd19ecb8591631a7b935ae0fedbf
Junjie Jiang <junjiejiang94@gmail.com>
2017-04-25 09:19:05 -0700

    test

--- models/models.js
+++ models/models.js
@@ -1,134 +1,28 @@
 var mongoose = require('mongoose');
 
-var userActionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+var contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
   },
-  eventCard: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'EventCard'
+  email: {
+    type: String,
+    require: true
   },
-  likeOrDislike: Boolean // true refers to like, false refers to dislike.
+  title: {
+    type: String,
+    require: true
+  },
+  content: {
+    type: String,
+    require: true
+  }
 }, {
   timestamps: true
 })
 
-var userSchema = new mongoose.Schema({
-  //How can we keep track of User Activity?
-  fname: {
-    type: String,
-    required: true
-  },
-  lname: {
-    type: String,
-    required: true
-  },
-  //Need to Hash Password
-  password: {
-    type: String
-  },
-  bio: {
-    type: String,
-    default: ""
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  image: [String],
-  videos: [String],
-  activeEvents: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'EventCard'
-  }],
-  connections: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  admin: {
-    type: Boolean,
-    default: false
-  },
-  rating: {
-    type: Number
-  }
-}, {
-  timestamps: true
-}
-);
-
-var eventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-  // required: true
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  // required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  category: String,
-  eventStartTime: String,
-  eventEndTime: String,
-  price: {
-    type: Number,
-    required: String
-  },
-  location: String,
-  image: String,
-  video: String,
-  usersAttending: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  // category: String
-  likes: [],
-  dislike: [],
-  pendingConnections: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }]
-}, {
-  timestamps: true
-});
-
-var messageSchema = new mongoose.Schema({
-  body: {
-    type: String,
-    required: true
-  },
-  toUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  fromUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  dateCreated: {
-    type: Date,
-    required: true
-  }
-
-});
-
-
-
-var Message = mongoose.model("Message", messageSchema);
-var User = mongoose.model("User", userSchema);
-var UserAction = mongoose.model("UserAction", userActionSchema);
-var EventCard = mongoose.model("EventCard", eventSchema)
+var Contact = mongoose.model("Contact", eventSchema)
 
 module.exports = {
-  Message: Message,
-  User: User,
-  UserAction: UserAction,
-  EventCard: EventCard
+  Contact: Contact
 };
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,15 @@
 var express = require('express'),
-    aws = require('aws-sdk'),
-    bodyParser = require('body-parser'),
-    multer = require('multer'),
-    multerS3 = require('multer-s3');
+  aws = require('aws-sdk'),
+  bodyParser = require('body-parser'),
+  multer = require('multer'),
+  multerS3 = require('multer-s3');
 var router = express.Router();
 
-var User  = require('../models/models').User;
-var Event  = require('../models/models').Event;
+var Contact = require('../models/models').Contact;
 
-
-router.get('/profile', function(req,res){
-
-
-	res.render('profile');
+router.get('/contact', function(req, res) {
+  res.render('contact');
 });
-
-router.get('/eventSwipe', function(req,res){
-	res.render('eventSwipe');
-});
-router.get('/login2', function(req,res){
-	res.render('login2');
-});
-router.get('/message', function(req,res){
-	res.render('message');
-});
-
 
 
 module.exports = router;
